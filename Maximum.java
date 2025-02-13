89be6c86ae2f4060ab219d414e18c8d6da48aaa5
roshan2004-nsr <roshanrohith1011@gmail.com>
2025-02-13 22:49:51 +0530

    Add files via upload

---
+++ Maximum.java
@@ -0,0 +1,9 @@
+public class Hotel
+{
+    int coffee=20;
+    int tea=10;
+    public static void main(String[] args) {
+        Hotel s1=new Hotel();
+        System.out.print(s1.coffee);
+    }
+}
