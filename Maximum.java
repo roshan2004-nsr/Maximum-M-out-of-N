b98d845ecaf3906acd2455ce2cb8e252ecc727f1
roshan2004-nsr <roshanrohith1011@gmail.com>
2025-02-13 22:51:15 +0530

    Update Maximum.java

--- Maximum.java
+++ Maximum.java
@@ -1,9 +1,32 @@
-public class Hotel
-{
-    int coffee=20;
-    int tea=10;
+import java.util.*;
+
+public class MaxSumMNumbers {
     public static void main(String[] args) {
-        Hotel s1=new Hotel();
-        System.out.print(s1.coffee);
+        Scanner sc = new Scanner(System.in);
+        
+        // Read N and M
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        
+        int[] arr = new int[N];
+        
+        // Read the N numbers
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        // Sort the array in descending order
+        Arrays.sort(arr);
+        
+        // Compute the sum of the largest M numbers
+        int maxSum = 0;
+        for (int i = N - 1; i >= N - M; i--) {
+            maxSum += arr[i];
+        }
+        
+        // Print the maximum sum
+        System.out.println(maxSum);
+        
+        sc.close();
     }
 }
